bd86b3bf7df479f42f0700528c3ecbc09daa6aec
Harshith Mysarla <harshimysarla@gmail.com>
2026-08-27 14:50:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (72.85%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int c=0;
+        int res=start^goal;
+        while(res!=0){
+            res=res&(res-1);
+            c++;
+        }
+        return c;
+        
+    }
+}
